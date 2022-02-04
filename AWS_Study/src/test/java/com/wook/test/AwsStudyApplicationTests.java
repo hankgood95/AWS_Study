8224dff4a6212faa5260bd7bd16bbeaa4f7ef433
hankgood95 <hankgood958@gmail.com>
2022-02-04 16:29:05 +0900

    aws 스터디용 프로젝트 올림

---
+++ AWS_Study/src/test/java/com/wook/test/AwsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wook.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
